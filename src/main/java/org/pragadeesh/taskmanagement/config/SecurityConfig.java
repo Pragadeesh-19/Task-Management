2ee9599aeb6842efed4ea89915db2bf2af247c36
pragadeesh <pragadeesh.online@gmail.com>
2024-11-18 14:01:12 +0530

    removed an import

--- src/main/java/org/pragadeesh/taskmanagement/config/SecurityConfig.java
+++ src/main/java/org/pragadeesh/taskmanagement/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.pragadeesh.taskmanagement.config;
 
 import lombok.RequiredArgsConstructor;
 import org.pragadeesh.taskmanagement.filter.JwtRequestFilter;
-import org.pragadeesh.taskmanagement.service.UserDetailsServiceImp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
